5f2abd767c13627f0a82fe0e688845f88e54fbb3
Adam McCormack <adamm@gwmail.gwu.edu>
2013-04-06 03:54:11 +0000

    final version

--- bash.sh
+++ bash.sh
@@ -7,35 +7,57 @@
 #      update the corresponding symlinks
 ################################################
 
-SYM_SRC=./ #File path to directory of symlinks
-F_SRC='/z/q/' #file path of files to be moved
-DEST=../x/y/ #destination of files
+SYM_SRC=$1 #File path to directory of symlinks
+F_SRC=$2 #file path of files to be moved
+DEST=$3 #destination of files
+
+if [ -z "$3" ]; then #checks if all arguments are entered
+  echo
+  echo Usage: $0 [symlink_directory] [orig_file_loc] [new_file_loc]
+  echo Example: $0 ./ /x/y/ ../z/q/
+  echo
+  exit
+elif [ ! -e "${DEST}" ]; then #checks if destination file path exists
+  echo
+  echo Error: \'$DEST\' is not a valid file path
+  echo
+  exit
+fi
+
+echo 
 
 # This loop will scan and pull out
 # each symbolic link that exist in the
 # directory $SYM_SRC
 for link in `find $SYM_SRC -type l`
-  do
-    # This loop will take all the symlinks
-    # and search for links that point to
-    # $F_SRC by using readlink to get the
-    # file path and grep to find the path
-    # matching $F_SRC
-    for path in `readlink $link | grep $F_SRC`
-      do
+do
+    
+  # $path gets the path of the symlink
+  # using 'readlink' and uses grep to
+  # to match it with $DEST
+  path=`readlink $link | grep $F_SRC`
 
-	echo $link points to $path 
-	echo 
-	file_name=${path##*/} #parses out file name from path
-	
-	echo moving $path to "${DEST}${file_name}"
-	echo
-        mv $path "${DEST}${file_name}" #moves file to new destination
-	
-	echo upating symlink $filename to point to "${DEST}${file_name}"
-	echo
-     	ln -sf "${DEST}${file_name}" $filename #updating symlink
-      done
+  if [ -n "$path"  ]; then #checks that $path is not null
+    
+    echo \'$link\' points to \'$path\' 
+    echo 
+
+    echo Moving \'$path\' to \'"${DEST}"\'
+    echo
+    mv $path "${DEST}" #moves file to new destination
+
+    file_name=${path##*/} #parses out file name from path
+
+    echo Upating symlink \'$filename\' to point to \'"${DEST}${file_name}"\'
+    echo
+    ln -sf "${DEST}${file_name}" $filename #updating symlink
+
+  fi
+
 done
 
-echo done!
+echo Job Completed.
+echo
+
+
+
